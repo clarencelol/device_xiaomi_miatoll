6a3513a35561feb553786f9c368be9bf2434ac25
NextWork123 <NextWork123@users.noreply.github.com>
2022-11-11 12:22:41 +0530

    miatoll: rootdir: Disable Chimera.GmsIntentOperationService - Seems like this is causing a weird overheating and battery drain, so let's disable it - Thanks to Peppe289 and Notzeeta
    Signed-off-by: popoA3M <pratyayaborborah@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -62,3 +62,5 @@ if [ -f /sys/class/kgsl/kgsl-3d0/gpu_available_frequencies ]; then
     gpu_freq=`cat /sys/class/kgsl/kgsl-3d0/gpu_available_frequencies` 2> /dev/null
     setprop vendor.gpu.available_frequencies "$gpu_freq"
 fi
+
+pm disable com.google.android.gms/.chimera.GmsIntentOperationService
