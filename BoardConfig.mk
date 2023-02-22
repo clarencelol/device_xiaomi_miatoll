d584308221451e225108c2f1d3c729c34c447fc1
Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
2023-02-22 20:03:19 +0800

    miatoll: Add diag hal interface
    Add diag hal interface for system client communication with vendor diag
    
    Change-Id: Iffd53443c1dcba4dc19168a0d932ddfca87f03d5
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,6 +69,7 @@ ART_BUILD_HOST_DEBUG := false
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
