55b1b94a6a3ab980a7bfcdb5931e35f7d6955d4d
clarencelol <clarencekuiek@icloud.com>
2023-04-28 20:11:15 +0800

    miatoll: Add support for Miui Camera
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: Giovanni Ricca <giovanniricca@proton.me>
    Co-authored-by: Nippon <adamayyad1999@hotmail.com>
    Co-authored-by: Alexander Winkowski <dereference23@outlook.com>
    Co-authored-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Co-authored-by: AndroidHQ254 <d.murimi14@gmail.com>
    Co-authored-by: neobuddy89 <neobuddy89@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -226,6 +226,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor \
