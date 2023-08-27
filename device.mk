4c4a549ee8bcbd91d54fb9255da1b866aca21bda
Ujjwal Kirorimal <ujjwalkirorimal@gmail.com>
2023-08-27 09:19:26 +0800

    miatoll: overlay: Disable Adaptive Connectivity preference
    Change-Id: I7274588c29cfd0078c168b7b7d9d36ef935e6bd0
    Signed-off-by: Clarence <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -285,6 +285,7 @@ PRODUCT_PACKAGES += \
     ApertureOverlay \
     CarrierConfigResCommon \
     SM6250Frameworks \
+    SM6250Settings \
     SM6250SystemUI \
     TelephonyResCommon \
     WifiOverlay
