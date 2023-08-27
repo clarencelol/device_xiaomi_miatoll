dc368c0effd08d5e4f53c68557cab3db02630de9
Carter Hsu <carterhsu@google.com>
2023-08-27 09:34:16 +0800

    miatoll: Update Audio HIDL to 7.1
    Bug: 205235270
    Test: build pass
    Signed-off-by: Carter Hsu <carterhsu@google.com>
    Change-Id: Ie1d8b20b001936e6c72ffe3644475c4fb8dad604
    Signed-off-by: Clarence <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@4.0-impl \
-    android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl
