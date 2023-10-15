e3857d879d9d91a06b45de35f7bd32f31d3e0c69
clarencelol <clarencekuiek@icloud.com>
2023-10-15 11:44:33 +0800

    miatoll: Build android.frameworks.sensorservice@1.0 into system
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -402,6 +402,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.multihal
 
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
