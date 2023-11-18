98ab88ee6e85711319f753eb4f3ba51a58dc651b
LuK1337 <priv.luk@gmail.com>
2023-11-18 23:01:23 +0800

    miatoll: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2 | AOSP
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -258,7 +258,8 @@ PRODUCT_PACKAGES += \
 
 # Workaround for prebuilt Qualcomm neural network HAL
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # NFC
 PRODUCT_PACKAGES += \
