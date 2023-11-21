72bab4ab02cff581e7273765600edbdd308fa71f
clarencelol <clarencekuiek@icloud.com>
2023-11-21 21:02:33 +0800

    miatoll: Add more prebuilt camera provider libs
    * Inspired by this https://review.lineageos.org/c/LineageOS/android_device_xiaomi_sm6250-common/+/374284
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -70,10 +70,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider-V1-ndk.vendor \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    android.hardware.camera.provider-V1-ndk.vendor
 
 PRODUCT_PACKAGES += \
     libpiex_shim
