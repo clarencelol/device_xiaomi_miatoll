54d5057e726da1c407ca82299424d4b0830c6ed0
clarencelol <clarencekuiek@icloud.com>
2023-12-11 16:37:36 +0800

    miatoll: PixelBuilds time
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common TequilaOS stuff.
-$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
+# Inherit some common PixelBuilds stuff.
+$(call inherit-product, vendor/pb/config/common_full_phone.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
@@ -17,9 +17,11 @@ $(call inherit-product, device/xiaomi/miatoll/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_USES_AOSP_RECOVERY := true
 
-PRODUCT_NAME := tequila_miatoll
+PRODUCT_NAME := aosp_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
