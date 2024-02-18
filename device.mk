a1ac8a27ee2f88cf4c71d3070abf92c345f7c46d
clarencelol <clarencekuiek@proton.me>
2024-02-18 22:45:50 +0800

    miatoll: Nuked pixel thermal
    * Switched over to .mock because QTI Thermal couldn't read temperature (bruh)
    
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- device.mk
+++ device.mk
@@ -473,11 +473,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.pixel \
-    thermal_symlinks
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+    android.hardware.thermal@2.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-camera.conf \
