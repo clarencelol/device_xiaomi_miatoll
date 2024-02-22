d30743c0594b1a7611f3aebb9532d491f2285d4d
Giovanni Ricca <giovanniricca@proton.me>
2024-02-22 20:08:48 +0800

    miatoll: Build goodix lib from source
    Change-Id: Ibaf1b9489b509318032c720c9c83e315cfe6ab3e | AOSP
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- device.mk
+++ device.mk
@@ -149,7 +149,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # FM
 PRODUCT_PACKAGES += \
